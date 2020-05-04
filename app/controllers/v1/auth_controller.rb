207623e2c7275632f39437122abbe014c733da58
Tales Godois <tales.godois@gmail.com>
2020-05-04 11:53:17 -0300

    handling exception on create user

--- app/controllers/v1/auth_controller.rb
+++ app/controllers/v1/auth_controller.rb
@@ -5,12 +5,18 @@ class V1::AuthController < ApplicationController
   param :email, String, desc: 'User Email'
   param :password, String, desc: 'User Password'
   def authenticate_user
-    user = User.find_for_database_authentication(email: params[:email])
-    if user.valid_password?(params[:password])
-      render json: payload(user)
-    else
-      render json: {errors: ['Invalid Username/Password']}, status: :unauthorized
+
+    begin
+      user = User.find_for_database_authentication(email: params[:email])
+      if user.valid_password?(params[:password])
+        render json: payload(user)
+      else
+        render json: {errors: ['Invalid Username/Password']}, status: :unauthorized
+      end
+    rescue => exception
+      render json: exception, status: :unprocessable_entity
     end
+
   end
 
   private
